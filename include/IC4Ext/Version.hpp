dc827dda18f81960f9173bf86ad920e54219ce82
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-10 18:06:34 +0900

    Add public v1.0.1 version constants

---
+++ include/IC4Ext/Version.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#define IC4EXT_VERSION_MAJOR 1
+#define IC4EXT_VERSION_MINOR 0
+#define IC4EXT_VERSION_PATCH 1
+#define IC4EXT_VERSION_STRING "1.0.1"
+
+namespace IC4Ext {
+
+inline constexpr int VersionMajor = IC4EXT_VERSION_MAJOR;
+inline constexpr int VersionMinor = IC4EXT_VERSION_MINOR;
+inline constexpr int VersionPatch = IC4EXT_VERSION_PATCH;
+inline constexpr const char* VersionString = IC4EXT_VERSION_STRING;
+
+} // namespace IC4Ext
