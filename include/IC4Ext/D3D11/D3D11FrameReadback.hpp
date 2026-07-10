50e4243e37b1641bc9294df44791393aa77b7832
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-10 10:23:54 +0900

    refactor: add D3D11Core-aware readback entry point

--- include/IC4Ext/D3D11/D3D11FrameReadback.hpp
+++ include/IC4Ext/D3D11/D3D11FrameReadback.hpp
@@ -4,6 +4,8 @@
 #include "IC4Ext/Core/Error.hpp"
 #include "IC4Ext/D3D11/D3D11CameraFrame.hpp"
 
+#include <D3D11Helper/D3D11Core/D3D11Core.hpp>
+
 #include <d3d11.h>
 #include <wrl/client.h>
 
@@ -14,6 +16,7 @@ namespace IC4Ext {
 class D3D11FrameReadback
 {
 public:
+    bool initialize(D3D11CoreLib::D3D11Core* core);
     bool initialize(ID3D11Device* device, ID3D11DeviceContext* context);
 
     bool readback(const D3D11CameraFrame& frame,
@@ -27,6 +30,7 @@ private:
     bool validateFrame(const D3D11CameraFrame& frame, GpuFrameFormat& srcFormat);
     void setError(ErrorCode code, const std::string& where, const std::string& message);
 
+    D3D11CoreLib::D3D11Core* core_ = nullptr;
     Microsoft::WRL::ComPtr<ID3D11Device> device_;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_;
     ErrorInfo lastError_;
