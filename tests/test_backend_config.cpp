4d07c88fa875b59f89c3b6361e1b79d7d4223db9
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-10 11:46:34 +0900

    Cover performance snapshot defaults in backend config test

--- tests/test_backend_config.cpp
+++ tests/test_backend_config.cpp
@@ -8,14 +8,30 @@ int main()
 #error "IC4Ext test target has no backend enabled"
 #endif
 
+    IC4Ext::CameraPerformanceSnapshot perfDefaults;
+    assert(!perfDefaults.streamStatistics.hasValue);
+    assert(!perfDefaults.timing.hasDeviceInterval);
+    assert(!perfDefaults.timing.hasHostInterval);
+    assert(perfDefaults.temperatures.empty());
+
 #if IC4EXT_ENABLE_D3D11
     IC4Ext::D3D11ReadyToken d3d11Invalid;
     assert(!d3d11Invalid.isValid());
+
+    IC4Ext::D3D11CameraCapture d3d11Capture;
+    auto d3d11Perf = d3d11Capture.performance();
+    assert(!d3d11Perf.streamStatistics.hasValue);
+    assert(d3d11Perf.captureStats.receivedBuffers == 0);
 #endif
 
 #if IC4EXT_ENABLE_D3D12
     IC4Ext::D3D12ReadyToken d3d12Invalid;
     assert(!d3d12Invalid.isValid());
+
+    IC4Ext::D3D12CameraCapture d3d12Capture;
+    auto d3d12Perf = d3d12Capture.performance();
+    assert(!d3d12Perf.streamStatistics.hasValue);
+    assert(d3d12Perf.captureStats.receivedBuffers == 0);
 #endif
 
     IC4Ext::CameraControlCommand cmd = IC4Ext::CameraControlCommand::ExposureTime(1234.0);
