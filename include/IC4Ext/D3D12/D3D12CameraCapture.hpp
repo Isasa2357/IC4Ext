c6db3156b8613005f013e31de053fb67a769f0fb
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-10 12:56:13 +0900

    Add D3D12 software trigger API

--- include/IC4Ext/D3D12/D3D12CameraCapture.hpp
+++ include/IC4Ext/D3D12/D3D12CameraCapture.hpp
@@ -77,6 +77,7 @@ public:
     bool setOffset(int offsetX, int offsetY) override;
     bool setRoi(int width, int height, int offsetX, int offsetY) override;
     bool setPixelFormat(CameraPixelFormat fmt) override;
+    bool softwareTrigger(const std::string& commandName = "TriggerSoftware") override;
 
     CameraCaptureStats stats() const;
     CameraPerformanceSnapshot performance();
