29193aab031890a2b106b748cf1cd31d8e950751
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 20:06:25 +0900

    Promote frame sync thread into D3D12 namespace

--- include/IC4Ext/D3D12/FrameSyncThread.hpp
+++ include/IC4Ext/D3D12/FrameSyncThread.hpp
@@ -2,10 +2,13 @@
 
 #include "IC4Ext/D3D12/FrameQueues.hpp"
 #include "IC4Ext/D3D12/FrameSyncOutputConfig.hpp"
+
+#define V2 D3D12
 #include "IC4Ext/V2/D3D12/D3D12FrameSyncThread.hpp"
+#undef V2
 
 namespace IC4Ext::D3D12 {
 
-using FrameSyncThread = ::IC4Ext::V2::D3D12FrameSyncThread;
+using FrameSyncThread = D3D12FrameSyncThread;
 
 } // namespace IC4Ext::D3D12
