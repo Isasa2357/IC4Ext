a50159b40e1292fdf050875e1ec23749b76fbadc
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:14:46 +0900

    Use semantic D3D12 API names in foundation test

--- tests/test_v2_d3d12_foundation.cpp
+++ tests/test_v2_d3d12_foundation.cpp
@@ -1,10 +1,4 @@
-#include "IC4Ext/V2/Core/FrameSyncOutputConfig.hpp"
-#include "IC4Ext/V2/Core/FrameSyncTypes.hpp"
-#include "IC4Ext/V2/D3D12/D3D12CameraCapture.hpp"
-#include "IC4Ext/V2/D3D12/D3D12CameraCaptureThread.hpp"
-#include "IC4Ext/V2/D3D12/D3D12FramePool.hpp"
-#include "IC4Ext/V2/D3D12/D3D12PooledFrameConverter.hpp"
-#include "IC4Ext/V2/D3D12/D3D12ReadOnlyFrameSet.hpp"
+#include "IC4Ext/D3D12/ReadOnlyPipeline.hpp"
 
 #include <cassert>
 #include <chrono>
@@ -14,16 +8,16 @@
 
 int main()
 {
-    using namespace IC4Ext::V2;
+    using namespace IC4Ext::D3D12;
 
-    static_assert(!std::is_copy_constructible_v<D3D12FrameWriter>);
-    static_assert(std::is_move_constructible_v<D3D12FrameWriter>);
-    static_assert(!std::is_copy_constructible_v<D3D12PooledFrameConverter>);
-    static_assert(std::is_move_constructible_v<D3D12PooledFrameConverter>);
-    static_assert(!std::is_copy_constructible_v<D3D12CameraCapture>);
-    static_assert(std::is_move_constructible_v<D3D12CameraCapture>);
-    static_assert(!std::is_copy_constructible_v<D3D12CameraCaptureThread>);
-    static_assert(!std::is_move_constructible_v<D3D12CameraCaptureThread>);
+    static_assert(!std::is_copy_constructible_v<FrameWriter>);
+    static_assert(std::is_move_constructible_v<FrameWriter>);
+    static_assert(!std::is_copy_constructible_v<PooledFrameConverter>);
+    static_assert(std::is_move_constructible_v<PooledFrameConverter>);
+    static_assert(!std::is_copy_constructible_v<CameraCapture>);
+    static_assert(std::is_move_constructible_v<CameraCapture>);
+    static_assert(!std::is_copy_constructible_v<CameraCaptureThread>);
+    static_assert(!std::is_move_constructible_v<CameraCaptureThread>);
 
     assert(FrameRateLimit::Maximum().isValid());
     assert(FrameRateLimit::Fixed(60.0).isValid());
@@ -37,7 +31,7 @@ int main()
     config.cameraIds = {0, 0};
     assert(!config.isValid());
 
-    D3D12FramePoolConfig poolConfig;
+    FramePoolConfig poolConfig;
     poolConfig.width = 1536;
     poolConfig.height = 1536;
     poolConfig.format = DXGI_FORMAT_R8G8B8A8_UNORM;
@@ -45,22 +39,22 @@ int main()
     poolConfig.createSrv = false;
     assert(!poolConfig.isValid());
 
-    D3D12CameraCaptureOptions captureOptions;
+    CameraCaptureOptions captureOptions;
     assert(captureOptions.isValid());
     captureOptions.maxFramePoolCapacity = 0;
     assert(!captureOptions.isValid());
 
-    D3D12CameraCaptureThreadOptions threadOptions;
+    CameraCaptureThreadOptions threadOptions;
     assert(threadOptions.isValid());
     threadOptions.readTimeoutMs = 0;
     assert(!threadOptions.isValid());
 
-    D3D12ReadOnlyFrameSet::FrameList frames;
-    frames.push_back(D3D12IndexedReadOnlyFrame{1, {}});
-    frames.push_back(D3D12IndexedReadOnlyFrame{0, {}});
+    ReadOnlyFrameSet::FrameList frames;
+    frames.push_back(IndexedReadOnlyFrame{1, {}});
+    frames.push_back(IndexedReadOnlyFrame{0, {}});
 
     const auto completed = std::chrono::steady_clock::now();
-    auto set = D3D12ReadOnlyFrameSet::Create(42, 123456, completed, std::move(frames));
+    auto set = ReadOnlyFrameSet::Create(42, 123456, completed, std::move(frames));
     assert(set.valid());
     assert(set.syncGroupId() == 42);
     assert(set.referenceTimestampNs() == 123456);
@@ -78,11 +72,11 @@ int main()
     output.priority = 100;
     assert(output.frameRate.isValid());
 
-    D3D12CameraCapture capture;
+    CameraCapture capture;
     assert(!capture.isOpened());
 
-    auto ingress = std::make_shared<D3D12IndexedReadOnlyFrameQueue>(4);
-    D3D12CameraCaptureThread captureThread(7, std::move(capture));
+    auto ingress = std::make_shared<IndexedReadOnlyFrameQueue>(4);
+    CameraCaptureThread captureThread(7, std::move(capture));
     captureThread.setOutputQueue(ingress);
     assert(captureThread.cameraId() == 7);
     assert(captureThread.outputQueue() == ingress);
