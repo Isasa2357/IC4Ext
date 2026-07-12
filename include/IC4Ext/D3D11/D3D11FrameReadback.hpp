a126b73115b959f91c1a6805f66f57f6084ab258
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-13 03:04:44 +0900

    Add D3D11 ReadOnly frame readback overload

--- include/IC4Ext/D3D11/D3D11FrameReadback.hpp
+++ include/IC4Ext/D3D11/D3D11FrameReadback.hpp
@@ -3,6 +3,7 @@
 #include "IC4Ext/Core/CpuFrame.hpp"
 #include "IC4Ext/Core/Error.hpp"
 #include "IC4Ext/D3D11/D3D11CameraFrame.hpp"
+#include "IC4Ext/D3D11/ReadOnlyFrame.hpp"
 
 #include <D3D11Helper/D3D11Core/D3D11Core.hpp>
 
@@ -24,27 +25,39 @@ public:
                   CpuFrame& out,
                   std::uint32_t waitTimeoutMs = 1000);
 
+    bool readback(const D3D11::ReadOnlyFrame& frame,
+                  CpuFrameFormat dstFormat,
+                  CpuFrame& out,
+                  std::uint32_t waitTimeoutMs = 1000);
+
     void resetCache() noexcept;
     FrameReadbackCacheStats cacheStats() const noexcept { return cacheStats_; }
-
     const ErrorInfo& lastError() const noexcept { return lastError_; }
 
 private:
-    bool validateFrame(const D3D11CameraFrame& frame,
-                       GpuFrameFormat& srcFormat,
-                       D3D11_TEXTURE2D_DESC& desc);
+    bool validateTexture(ID3D11Texture2D* texture,
+                         const char* where,
+                         GpuFrameFormat& srcFormat,
+                         D3D11_TEXTURE2D_DESC& desc);
     bool ensureStagingTexture(const D3D11_TEXTURE2D_DESC& sourceDesc);
-    void setError(ErrorCode code, const std::string& where, const std::string& message);
+    bool copyAndConvert(ID3D11Texture2D* texture,
+                        const D3D11_TEXTURE2D_DESC& desc,
+                        GpuFrameFormat srcFormat,
+                        CpuFrameFormat dstFormat,
+                        const FrameTiming& timing,
+                        const FrameChunkMetadata& chunkMetadata,
+                        CpuFrame& out);
+    void setError(ErrorCode code,
+                  const std::string& where,
+                  const std::string& message);
 
     D3D11CoreLib::D3D11Core* core_ = nullptr;
     Microsoft::WRL::ComPtr<ID3D11Device> device_;
     Microsoft::WRL::ComPtr<ID3D11DeviceContext> context_;
-
     Microsoft::WRL::ComPtr<ID3D11Texture2D> stagingTexture_;
     D3D11_TEXTURE2D_DESC stagingDesc_{};
     bool hasStagingDesc_ = false;
     FrameReadbackCacheStats cacheStats_{};
-
     ErrorInfo lastError_;
 };
 
