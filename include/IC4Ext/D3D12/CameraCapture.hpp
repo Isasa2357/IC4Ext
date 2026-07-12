805f2e00596863a8baad2b6df63fa36abffd106d
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 20:06:02 +0900

    Promote camera capture into D3D12 namespace

--- include/IC4Ext/D3D12/CameraCapture.hpp
+++ include/IC4Ext/D3D12/CameraCapture.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
 #include "IC4Ext/D3D12/FramePool.hpp"
+#include "IC4Ext/D3D12/PooledFrameConverter.hpp"
+
+#define V2 D3D12
 #include "IC4Ext/V2/D3D12/D3D12CameraCapture.hpp"
+#undef V2
 
 namespace IC4Ext::D3D12 {
 
-using CameraCaptureOptions = ::IC4Ext::V2::D3D12CameraCaptureOptions;
-using ReadResult = ::IC4Ext::V2::D3D12ReadOnlyReadResult;
-using CameraCapture = ::IC4Ext::V2::D3D12CameraCapture;
+using CameraCaptureOptions = D3D12CameraCaptureOptions;
+using ReadResult = D3D12ReadOnlyReadResult;
+using CameraCapture = D3D12CameraCapture;
 
 } // namespace IC4Ext::D3D12
