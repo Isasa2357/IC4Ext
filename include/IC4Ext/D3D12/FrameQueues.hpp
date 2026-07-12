beb428916a41b64a5d0869fcca51911684dd1719
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-13 01:41:19 +0900

    Move D3D12 read-only frame queue aliases out of V2 wrapper

--- include/IC4Ext/D3D12/FrameQueues.hpp
+++ include/IC4Ext/D3D12/FrameQueues.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include "IC4Ext/D3D12/FrameSyncTypes.hpp"
+#include "IC4Ext/D3D12/ReadOnlyFrame.hpp"
 #include "IC4Ext/D3D12/ReadOnlyFrameSet.hpp"
 
-#define V2 D3D12
-#include "IC4Ext/V2/D3D12/D3D12FrameQueues.hpp"
-#undef V2
+#include <ThreadKit/Queues/BlockingQueue.hpp>
 
 namespace IC4Ext::D3D12 {
+
+struct D3D12IndexedReadOnlyCameraFrame
+{
+    CameraId cameraId = 0;
+    D3D12ReadOnlyFrame frame;
+};
+
+using D3D12IndexedReadOnlyFrameQueue =
+    ThreadKit::Queues::BlockingQueue<D3D12IndexedReadOnlyCameraFrame>;
+
+using D3D12ReadOnlyFrameSetQueue =
+    ThreadKit::Queues::BlockingQueue<D3D12ReadOnlyFrameSet>;
 
 using IndexedReadOnlyCameraFrame = D3D12IndexedReadOnlyCameraFrame;
 using IndexedReadOnlyFrameQueue = D3D12IndexedReadOnlyFrameQueue;
