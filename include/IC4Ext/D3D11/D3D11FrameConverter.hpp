cbf1518414bee676365b09792765c175332c72f5
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-13 03:55:08 +0900

    Protect legacy D3D11 conversion transactions

--- include/IC4Ext/D3D11/D3D11FrameConverter.hpp
+++ include/IC4Ext/D3D11/D3D11FrameConverter.hpp
@@ -4,6 +4,7 @@
 #include "IC4Ext/Core/CoreTypes.hpp"
 #include "IC4Ext/Core/Error.hpp"
 #include "IC4Ext/D3D11/D3D11CameraFrame.hpp"
+#include "IC4Ext/D3D11/D3D11ContextSynchronization.hpp"
 
 #include <D3D11Helper/D3D11Core/D3D11Core.hpp>
 #include <D3D11Helper/D3D11Gpu/D3D11ComputePipeline.hpp>
@@ -14,6 +15,7 @@
 #include <wrl/client.h>
 #include <cstdint>
 #include <memory>
+#include <mutex>
 #include <string>
 #include <vector>
 
@@ -77,6 +79,7 @@ private:
     D3D11CoreLib::D3D11Core* core_ = nullptr;
     ID3D11Device* device_ = nullptr;
     ID3D11DeviceContext* context_ = nullptr;
+    std::shared_ptr<std::recursive_mutex> contextMutex_;
     D3D11FenceManager* fenceManager_ = nullptr;
     ShaderLoadConfig shaderConfig_;
     ErrorInfo lastError_;
