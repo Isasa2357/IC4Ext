daad570bd7f426f591470be8cac73f1757094f18
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:14:01 +0900

    Add semantic D3D12 queue aliases

---
+++ include/IC4Ext/D3D12/FrameQueues.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "IC4Ext/D3D12/ReadOnlyFrameSet.hpp"
+#include "IC4Ext/V2/D3D12/D3D12FrameQueues.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using IndexedReadOnlyCameraFrame = ::IC4Ext::V2::D3D12IndexedReadOnlyCameraFrame;
+using IndexedReadOnlyFrameQueue = ::IC4Ext::V2::D3D12IndexedReadOnlyFrameQueue;
+using ReadOnlyFrameSetQueue = ::IC4Ext::V2::D3D12ReadOnlyFrameSetQueue;
+
+} // namespace IC4Ext::D3D12
