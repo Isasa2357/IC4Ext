77a17b3acbbff05cdc8967dca9c219898265e5d7
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:13:26 +0900

    Add semantic D3D12 output config aliases

---
+++ include/IC4Ext/D3D12/FrameSyncOutputConfig.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "IC4Ext/D3D12/FrameSyncTypes.hpp"
+#include "IC4Ext/V2/Core/FrameSyncOutputConfig.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using FrameSyncOutputConfig = ::IC4Ext::V2::FrameSyncOutputConfig;
+using FrameSyncOutputInfo = ::IC4Ext::V2::FrameSyncOutputInfo;
+using FrameSyncOutputStats = ::IC4Ext::V2::FrameSyncOutputStats;
+
+} // namespace IC4Ext::D3D12
