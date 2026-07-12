8303a0b3b2e16953139cb1134b28d5963594a165
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:14:16 +0900

    Add semantic D3D12 capture thread aliases

---
+++ include/IC4Ext/D3D12/CameraCaptureThread.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "IC4Ext/D3D12/CameraCapture.hpp"
+#include "IC4Ext/D3D12/FrameQueues.hpp"
+#include "IC4Ext/V2/D3D12/D3D12CameraCaptureThread.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using CameraCaptureThreadOptions = ::IC4Ext::V2::D3D12CameraCaptureThreadOptions;
+using CameraCaptureThreadStats = ::IC4Ext::V2::D3D12CameraCaptureThreadStats;
+using CameraCaptureThread = ::IC4Ext::V2::D3D12CameraCaptureThread;
+
+} // namespace IC4Ext::D3D12
