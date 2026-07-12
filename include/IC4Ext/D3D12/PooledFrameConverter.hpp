5e226371ff9e046596f445e64e3fe8f205fd1e41
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:13:47 +0900

    Add semantic D3D12 pooled converter alias

---
+++ include/IC4Ext/D3D12/PooledFrameConverter.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "IC4Ext/D3D12/FramePool.hpp"
+#include "IC4Ext/V2/D3D12/D3D12PooledFrameConverter.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using PooledFrameConverter = ::IC4Ext::V2::D3D12PooledFrameConverter;
+
+} // namespace IC4Ext::D3D12
