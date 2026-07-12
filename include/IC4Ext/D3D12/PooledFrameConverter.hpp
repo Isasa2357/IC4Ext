10a95962687030cb128059d9c2d4c35d6dc57cf3
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 20:05:53 +0900

    Promote pooled frame converter into D3D12 namespace

--- include/IC4Ext/D3D12/PooledFrameConverter.hpp
+++ include/IC4Ext/D3D12/PooledFrameConverter.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "IC4Ext/D3D12/FramePool.hpp"
+
+#define V2 D3D12
 #include "IC4Ext/V2/D3D12/D3D12PooledFrameConverter.hpp"
+#undef V2
 
 namespace IC4Ext::D3D12 {
 
-using PooledFrameConverter = ::IC4Ext::V2::D3D12PooledFrameConverter;
+using PooledFrameConverter = D3D12PooledFrameConverter;
 
 } // namespace IC4Ext::D3D12
