5dd2cafebb299c16a16a4051fca2efb177a1fc5c
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:14:07 +0900

    Add semantic D3D12 camera capture aliases

---
+++ include/IC4Ext/D3D12/CameraCapture.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "IC4Ext/D3D12/FramePool.hpp"
+#include "IC4Ext/V2/D3D12/D3D12CameraCapture.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using CameraCaptureOptions = ::IC4Ext::V2::D3D12CameraCaptureOptions;
+using ReadResult = ::IC4Ext::V2::D3D12ReadOnlyReadResult;
+using CameraCapture = ::IC4Ext::V2::D3D12CameraCapture;
+
+} // namespace IC4Ext::D3D12
