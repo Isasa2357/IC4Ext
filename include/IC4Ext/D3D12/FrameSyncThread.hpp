7ef445192ea247e2d99a6cf6f1a7871e2317e37a
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:14:22 +0900

    Add semantic D3D12 sync thread alias

---
+++ include/IC4Ext/D3D12/FrameSyncThread.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "IC4Ext/D3D12/FrameQueues.hpp"
+#include "IC4Ext/D3D12/FrameSyncOutputConfig.hpp"
+#include "IC4Ext/V2/D3D12/D3D12FrameSyncThread.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using FrameSyncThread = ::IC4Ext::V2::D3D12FrameSyncThread;
+
+} // namespace IC4Ext::D3D12
