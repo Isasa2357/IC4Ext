2774225ae842484a4dda1e1dc0f5485cf17eff0d
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 20:05:04 +0900

    Promote read-only frame set into D3D12 namespace

--- include/IC4Ext/D3D12/ReadOnlyFrameSet.hpp
+++ include/IC4Ext/D3D12/ReadOnlyFrameSet.hpp
@@ -2,11 +2,14 @@
 
 #include "IC4Ext/D3D12/FrameSyncTypes.hpp"
 #include "IC4Ext/D3D12/ReadOnlyFrame.hpp"
+
+#define V2 D3D12
 #include "IC4Ext/V2/D3D12/D3D12ReadOnlyFrameSet.hpp"
+#undef V2
 
 namespace IC4Ext::D3D12 {
 
-using IndexedReadOnlyFrame = ::IC4Ext::V2::D3D12IndexedReadOnlyFrame;
-using ReadOnlyFrameSet = ::IC4Ext::V2::D3D12ReadOnlyFrameSet;
+using IndexedReadOnlyFrame = D3D12IndexedReadOnlyFrame;
+using ReadOnlyFrameSet = D3D12ReadOnlyFrameSet;
 
 } // namespace IC4Ext::D3D12
