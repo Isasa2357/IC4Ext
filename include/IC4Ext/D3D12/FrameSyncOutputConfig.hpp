b4b1473e970da636c6fe566d83360c1d83f5b905
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-13 01:40:11 +0900

    Move D3D12 frame sync output config out of V2 wrapper

--- include/IC4Ext/D3D12/FrameSyncOutputConfig.hpp
+++ include/IC4Ext/D3D12/FrameSyncOutputConfig.hpp
@@ -2,12 +2,33 @@
 
 #include "IC4Ext/D3D12/FrameSyncTypes.hpp"
 
-#define V2 D3D12
-#include "IC4Ext/V2/Core/FrameSyncOutputConfig.hpp"
-#undef V2
+#include <cstdint>
+#include <vector>
 
 namespace IC4Ext::D3D12 {
 
-// Types are defined by the implementation header directly in IC4Ext::D3D12.
+struct FrameSyncOutputConfig
+{
+    std::vector<CameraId> requiredCameras;
+    FrameRateLimit frameRate = FrameRateLimit::Maximum();
+    std::int32_t priority = 0;
+    bool enabled = true;
+};
+
+struct FrameSyncOutputInfo
+{
+    FrameSyncOutputId id = InvalidFrameSyncOutputId;
+    FrameSyncOutputConfig config;
+    std::uint64_t registrationOrder = 0;
+};
+
+struct FrameSyncOutputStats
+{
+    std::uint64_t consideredSets = 0;
+    std::uint64_t skippedByFrameRate = 0;
+    std::uint64_t emittedSets = 0;
+    std::uint64_t queueDrops = 0;
+    std::uint64_t disabledSkips = 0;
+};
 
 } // namespace IC4Ext::D3D12
