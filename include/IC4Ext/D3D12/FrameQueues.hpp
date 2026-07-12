b8e4d15774da9455f89d8fc7de603c1ca5bdab0e
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 20:05:14 +0900

    Promote read-only frame queues into D3D12 namespace

--- include/IC4Ext/D3D12/FrameQueues.hpp
+++ include/IC4Ext/D3D12/FrameQueues.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "IC4Ext/D3D12/ReadOnlyFrameSet.hpp"
+
+#define V2 D3D12
 #include "IC4Ext/V2/D3D12/D3D12FrameQueues.hpp"
+#undef V2
 
 namespace IC4Ext::D3D12 {
 
-using IndexedReadOnlyCameraFrame = ::IC4Ext::V2::D3D12IndexedReadOnlyCameraFrame;
-using IndexedReadOnlyFrameQueue = ::IC4Ext::V2::D3D12IndexedReadOnlyFrameQueue;
-using ReadOnlyFrameSetQueue = ::IC4Ext::V2::D3D12ReadOnlyFrameSetQueue;
+using IndexedReadOnlyCameraFrame = D3D12IndexedReadOnlyCameraFrame;
+using IndexedReadOnlyFrameQueue = D3D12IndexedReadOnlyFrameQueue;
+using ReadOnlyFrameSetQueue = D3D12ReadOnlyFrameSetQueue;
 
 } // namespace IC4Ext::D3D12
