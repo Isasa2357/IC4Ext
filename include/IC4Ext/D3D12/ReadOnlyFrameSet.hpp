ccf34a9bb7bb653c104322ca95dbef68c95c948f
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:13:54 +0900

    Add semantic D3D12 frame set aliases

---
+++ include/IC4Ext/D3D12/ReadOnlyFrameSet.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "IC4Ext/D3D12/FrameSyncTypes.hpp"
+#include "IC4Ext/D3D12/ReadOnlyFrame.hpp"
+#include "IC4Ext/V2/D3D12/D3D12ReadOnlyFrameSet.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using IndexedReadOnlyFrame = ::IC4Ext::V2::D3D12IndexedReadOnlyFrame;
+using ReadOnlyFrameSet = ::IC4Ext::V2::D3D12ReadOnlyFrameSet;
+
+} // namespace IC4Ext::D3D12
