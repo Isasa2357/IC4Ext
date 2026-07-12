87ec389d27ff0600bb79ca40ae49c665cbf7a012
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 19:13:33 +0900

    Add semantic D3D12 read-only frame alias

---
+++ include/IC4Ext/D3D12/ReadOnlyFrame.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "IC4Ext/V2/D3D12/D3D12ReadOnlyFrame.hpp"
+
+namespace IC4Ext::D3D12 {
+
+using ReadOnlyFrame = ::IC4Ext::V2::D3D12ReadOnlyFrame;
+
+} // namespace IC4Ext::D3D12
