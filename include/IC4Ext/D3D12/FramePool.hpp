ef9ec15d612dbd25e08bd413d11fc2f85b92d000
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-12 20:05:33 +0900

    Promote frame pool into D3D12 namespace

--- include/IC4Ext/D3D12/FramePool.hpp
+++ include/IC4Ext/D3D12/FramePool.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "IC4Ext/D3D12/ReadOnlyFrame.hpp"
+
+#define V2 D3D12
 #include "IC4Ext/V2/D3D12/D3D12FramePool.hpp"
+#undef V2
 
 namespace IC4Ext::D3D12 {
 
-using FramePoolExhaustionPolicy = ::IC4Ext::V2::FramePoolExhaustionPolicy;
-using FramePoolConfig = ::IC4Ext::V2::D3D12FramePoolConfig;
-using FramePoolStats = ::IC4Ext::V2::D3D12FramePoolStats;
-using FrameWriter = ::IC4Ext::V2::D3D12FrameWriter;
-using FramePool = ::IC4Ext::V2::D3D12FramePool;
+using FramePoolExhaustionPolicy = FramePoolExhaustionPolicy;
+using FramePoolConfig = D3D12FramePoolConfig;
+using FramePoolStats = D3D12FramePoolStats;
+using FrameWriter = D3D12FrameWriter;
+using FramePool = D3D12FramePool;
 
 } // namespace IC4Ext::D3D12
