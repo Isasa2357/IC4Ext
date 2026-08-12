d2894ba69611b8866cc9274fcbcfba7929309727
Isasa2357 <Isasa_EGwork@outlook.com>
2026-08-12 19:51:17 +0900

    Align public version header with 2.0.0

--- include/IC4Ext/Version.hpp
+++ include/IC4Ext/Version.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#define IC4EXT_VERSION_MAJOR 1
+#define IC4EXT_VERSION_MAJOR 2
 #define IC4EXT_VERSION_MINOR 0
-#define IC4EXT_VERSION_PATCH 1
-#define IC4EXT_VERSION_STRING "1.0.1"
+#define IC4EXT_VERSION_PATCH 0
+#define IC4EXT_VERSION_STRING "2.0.0"
 
 namespace IC4Ext {
 
